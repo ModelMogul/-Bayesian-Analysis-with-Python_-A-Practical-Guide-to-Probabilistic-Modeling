757ea2d9510f28ed4d5f8dd1354212e74dba6df4
Troy Sun <troysun@Troys-MacBook-Pro.local>
2024-10-20 15:01:35 -0400

    start using pm

--- Chapter 2.py
+++ Chapter 2.py
@@ -1,5 +1,8 @@
+<<<<<<< HEAD
 print('hello')
 print('hopefully this will work')
 print('yes it works!!')
 print('let`s do a pull request')
 print('this is for chapter 2')
+=======
+>>>>>>> d893c4e (start chapter2)
